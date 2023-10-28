bd4299b98e7f6af04072b012d64dc25ecdfff190
PhuNH <1079742+PhuNH@users.noreply.github.com>
2023-10-28 16:20:24 +0200

    Implement /new-imp endpoint for bidder
    The bidder calculates its bid and sends that back to the bidnet. The bidnet prints out what it receives.

--- bidder/bidder/routes.py
+++ bidder/bidder/routes.py
@@ -1,6 +1,23 @@
-from . import app
+from flask import request
+
+from . import app, db
+from .models import Impression
 
 
 @app.route('/')
 def index():
     return 'Welcome to bidder'
+
+
+def calc_bid(imp_profile: str) -> int:
+    bidder_profile = app.config['PROFILE']
+    print(f"Bidder: '{bidder_profile}'; Impression: '{imp_profile}'")
+    return 10
+
+
+@app.route('/new-imp', methods=['POST'])
+def receive_imp():
+    imp_profile = request.data.decode('ascii')
+    bid = calc_bid(imp_profile)
+    # db.session.add(Impression(profile=imp_profile, bid=bid))
+    return str(bid)
--- bidnet/bidnet/jobs.py
+++ bidnet/bidnet/jobs.py
@@ -6,5 +6,9 @@ from .models import Impression
 
 def job_1():
     i = Impression()
-    requests.post(f"{app.config['BIDDER_ADDR']}new-imp", i.profile)
     print(i)
+
+    response = requests.post(f"{app.config['BIDDER_ADDR']}new-imp", i.profile)
+    if response.status_code == requests.codes.ok:
+        bid = response.content
+        print(f'Received bid of {bid}')
