ade0493f319ab8abc7b0ebd410032e9faa01a644
PhuNH <1079742+PhuNH@users.noreply.github.com>
2023-10-28 18:18:53 +0200

    Stop bidder from auto-starting bidnet

--- bidder/bidder/__init__.py
+++ bidder/bidder/__init__.py
@@ -1,5 +1,3 @@
-import requests
-
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
@@ -14,10 +12,6 @@ db.init_app(app)
 migrate = Migrate(app, db)
 
 from . import routes, models
-
-response = requests.get(f"{app.config['BIDNET_ADDR']}start")
-if response.status_code == requests.codes.ok:
-    print(f"bidnet: {response.json().get('job')} created")
 
 
 @app.shell_context_processor
